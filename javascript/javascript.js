d441f749472704a206be93ba6848223eed2a7d31
Steve Maillardet <steve.maillardet@btinternet.com>
2019-02-15 07:41:07 +0000

    Added two more songs

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,59 +1,56 @@
-var audio_bacchus, playbtn_bacchus, audio_bacchus, playbtn_bacchus, bacchus_audio_playing = false, nomad_audio_playing = false;
+var audio, playbtn = [], audio_playing;
+var audio_clips = ["audio/Bacchus_clip.mp3", "audio/Nomad_clip.mp3", "audio/Reset_My_Year_clip.mp3", "audio/Thin_White_Duke_clip.mp3"];
+
 function initAudioPlayer(){
-	audio_bacchus = new Audio();
-	audio_bacchus.src = "audio/Bacchus_clip.mp3";
-	audio_nomad = new Audio();
-	audio_nomad.src = "audio/Nomad_clip.mp3";
+	audio = new Audio();
+	audio_playing = 0;
+
 	// set object references
-	playbtn_bacchus = document.getElementById("bacchusplaybtn");
-	playbtn_nomad = document.getElementById("nomadplaybtn");
-	// Add Event Handling
-	playbtn_bacchus.addEventListener("click",playPause_bacchus);
-	playbtn_nomad.addEventListener("click",playPause_nomad);
-	audio_bacchus.addEventListener("ended",reset_playbtns);
-	audio_nomad.addEventListener("ended",reset_playbtns);
+	playbtn[0] = document.getElementById("bacchusplaybtn");
+	playbtn[1] = document.getElementById("nomadplaybtn");
+	playbtn[2] = document.getElementById("reset_my_yearplaybtn");
+	playbtn[3] = document.getElementById("thin_white_dukeplaybtn");
+	
+	// Add Event Handling.  Note: Cannot use a for loop as function playPause stored as text
+	playbtn[0].addEventListener("click", function() {playPause(0);});
+	playbtn[1].addEventListener("click", function() {playPause(1);});
+	playbtn[2].addEventListener("click", function() {playPause(2);});
+	playbtn[3].addEventListener("click", function() {playPause(3);});
+		
+	audio.addEventListener("ended",reset_playbtns);
 
 	// Functions
-	function playPause_bacchus () {
-		// if another audio file already playing, don't play
-		if(nomad_audio_playing)
-			return;
-		// Play or Stop Bacchus Audio file
-		if(!bacchus_audio_playing) {
-			audio_bacchus.play();
-			playbtn_bacchus.style.background = "url(img/button_black_stop.png) no-repeat";
+	function playPause(buttonid) 
+	{
+		// if no audio playing play the track	
+		if(audio_playing == 0) 
+			{
+			audio.src = audio_clips[buttonid]; 
+			audio.play();
+			playbtn[buttonid].style.background = "url(img/button_black_stop.png) no-repeat";
+			audio_playing = buttonid+1;
 			}
-		else {
-			audio_bacchus.pause();
-			audio_bacchus.currentTime = 0
-			playbtn_bacchus.style.background = "url(img/button_grey_play.png) no-repeat";
+		// if audio playing and the same button is pressed again, then stop the track	
+		else if(audio_playing == buttonid+1)
+			{
+			audio.pause();
+			audio.currentTime = 0
+			playbtn[buttonid].style.background = "url(img/button_grey_play.png) no-repeat";
+			audio_playing = 0;
 			}
-		bacchus_audio_playing = !bacchus_audio_playing;
 	}
 
-	function playPause_nomad () {
-		// If another audio file already playing, don't play
-		if(bacchus_audio_playing)
-			return;
-		// Play or Stop Nomad Audio file
-		if(!nomad_audio_playing) {
-			audio_nomad.play();
-			playbtn_nomad.style.background = "url(img/button_black_stop.png) no-repeat";
-			}
-		else {
-			audio_nomad.pause();
-			audio_nomad.currentTime = 0
-			playbtn_nomad.style.background = "url(img/button_grey_play.png) no-repeat";
-			}
-		nomad_audio_playing = !nomad_audio_playing;
-	}
+	
 
 	// if end of audio file detected, reset all playbtns to default
-	function reset_playbtns () {
-		playbtn_bacchus.style.background = "url(img/button_grey_play.png) no-repeat"; 
-		playbtn_nomad.style.background = "url(img/button_grey_play.png) no-repeat"
-		bacchus_audio_playing = false;
-		nomad_audio_playing = false;
+	function reset_playbtns () 
+	{
+		var i;
+		for ( i = 0; i < audio_clips.length; i++ )
+			{
+			playbtn[i].style.background = "url(img/button_grey_play.png) no-repeat";
+			} 
+		audio_playing = 0;
 	}
 
 }
